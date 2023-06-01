180a414d0396146c09f49b61652917101682fe91
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-06-01 15:55:37 +0300

    finally

---
+++ src/components/Overlay/Overlay.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  background: rgba(0, 0, 0, 0.6);
+`;
