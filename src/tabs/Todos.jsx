7cff9a128ab0ff9361a6aef1d7e7c59f6c864ff7
ginkosoul <ginkosoul@gmail.com>
2023-06-05 15:15:26 +0300

    added delete button

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,21 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
+const LS_KEY = 'todos';
 export const Todos = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useLocalStorage(LS_KEY, []);
   const onSubmit = text => {
     const todo = { text, id: nanoid() };
+    const isExist = todos.some(
+      e => e.text.toLowerCase() === text.toLowerCase()
+    );
+    if (isExist) {
+      alert(`Todo ${text} already exists`);
+      return;
+    }
     setTodos(prevTodos => [...prevTodos, todo]);
   };
 
+  const handleDelete = id => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  };
   return (
     <>
       <SearchForm onSubmit={onSubmit} />
       <Grid>
         {todos.map((todo, index) => (
           <GridItem key={todo.id}>
-            <Todo id={todo.id} text={todo.text} counter={index + 1} />
+            <Todo
+              onDelete={handleDelete}
+              id={todo.id}
+              text={todo.text}
+              counter={index + 1}
+            />
           </GridItem>
         ))}
       </Grid>
